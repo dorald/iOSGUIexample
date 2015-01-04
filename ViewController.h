a039fab2d5a386fadbd0015ed8ef106148ae5812
Dorald <doraldd@gmail.com>
2015-01-05 00:09:45 +0100

    ...

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSGUIexample
+//
+//  Created by Dorald on 1/4/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#include "ofApp.h"
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ViewController.mm
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  iOSGUIexample
+//
+//  Created by Dorald on 1/4/15.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+ofApp * myApp;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    myApp = (ofApp*)ofGetAppPtr();
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
